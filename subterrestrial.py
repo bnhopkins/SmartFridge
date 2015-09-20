b94d4b9b0acc66e267656df3651efbaa40dff01f
bnhopkins <bnhopkins@mac.com>
2015-09-20 02:40:03 -0400

    updated functions

--- subterrestrial.py
+++ subterrestrial.py
@@ -4,15 +4,16 @@ from PIL import Image
 from serialCom import dState
 
 print "Welcome to Sub Terrestrial"
- 
-foodInTheFridge = [[0,0],[0,0],[0,0]]
-tags=[]
 
-def defineFood(newfood):
+#inventory list
+foodInTheFridge = [[0,0],[0,0],[0,0]]
+
+
+def defineFood(fooditem):
 	"""Send picture to clarafai, return tags"""
 	tags=[]
 	clarifai_api = ClarifaiApi(app_id='tbndTvx-Mv_OGD4CKeOhPap1gfAFSSWDUzPT2X6x', app_secret='JQNnSLBftBwJLMkNtIsdhUdU7OQ0a5HZDKLdPTtR') # assumes environment variables are set.
-	result = clarifai_api.tag_images(newfood)
+	result = clarifai_api.tag_images(fooditem)
 	#parse result
 	i=0
 	while (i<len(result[u'results'][0][u'result'][u'tag'][u'classes'])):
@@ -26,7 +27,7 @@ def setFoodName():
 	#############################
 
 def setCoord():
-	"""find empty item bin number"""
+	"""find empty item bin number to store food item, return coordinates """
 	row=0
 	for rows in foodInTheFridge:
 		column=1
@@ -39,23 +40,32 @@ def setCoord():
 
 def addFood():
 	"""add the food tags and number to the fridge dictionary"""
+	#set coordinates of food
 	itemCoord=foodItemNum() #item coordinates in fridge table
-	foodInTheFridge[row, column] = getChoice() # store string of food name chosen by user in the fridge inventory
-	row= char(itemCoord[0]+97) #convert row into alphabet for serialCom
+	row=itemCoord[0]
 	column=itemCoord[1] #column for serialCom
-	dState('L', row, column, 1)
+	#store item in list
+	foodInTheFridge[itemCoord[0], column] = getChoice() # store string of food name chosen by user in the fridge inventory
+	rowLetter= char(row+97) #convert row into alphabet for serialCom
+	#send food item to hardware to light up place in fridge
+	dState('L', rowLetter, column, 1)
+
 
 def returnFridgeFoodList():
 	"""return list of food available in fridge"""
-	return foodInTheFridge
+	foodList=[]
+	box=1
+	for rows in foodInTheFridge:
+		for items in rows:
+			if (items!=0):
+				foodList.append(item)
+	return foodList
 
 def getChoice():
 	##impliment ################
 	return str "banana"
 	############################
 
-#def removeItem();
-#	"""remove item from the inventory of foodInTheFridge"""
 def getCoord(requestedItem):
 	row=0
 	for rows in foodInTheFridge:
@@ -66,17 +76,16 @@ def getCoord(requestedItem):
 			column= column+1
 		row= row +1		
 
-def getFood(requestedItem):
+def removeFood(requestedItem):
 	""""search for the food in the fridge and return bin number"""
-	itemCoord=
-	dState('S',row,colum,)
+	itemCoord = getCoord(requestedItem)
+	row = itemCoord[0]
+	column = itemCoord[1]
+	foodInTheFridge[row, column] = 0
+	rowLetter = char(row+97)
+	dState('S',rowLetter, column,'1')
 
 
-
-#thisFood = open('banana.jpg')
-#print(defineFood(thisFood))
-
-foodItemNum()
-
-
-#takePicture()
+##run code- get item and remove item from fridge
+addFood()
+removeFood()
