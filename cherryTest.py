ffc33b5e744fe531e30c31cd54f72f243e70cfe7
mslos <ms7576@nyu.edu>
2015-09-19 23:55:05 -0400

    Cherry pi experiment

--- cherryTest.py
+++ cherryTest.py
@@ -4,6 +4,9 @@ import cherrypy
 import cgi
 import tempfile
 import os
+from SimpleCV import Camera
+from clarifai.client import ClarifaiApi
+from PIL import Image
 
 
 __author__ = "Ex Vito"
@@ -14,6 +17,49 @@ __author__ = "Ex Vito"
 cherrypy.config.update({
                         'server.socket_port': 8000,
                        })
+
+foodInTheFridge = [[0,0],[0,0],[0,0]]
+# jpgdata = newFood.read()
+# newFood.close()
+# ##figure out gui
+
+# # ## AddFood item
+# def takePicture():
+# 	"""take picture of the food to add to inventory"""
+# 	cam = Camera()
+# 	newFood = cam.getImage()
+# 	newFood.save("newFood.png")
+# 	return newFood
+
+def defineFood(newfood):
+	"""Send picture to clarafai, return tags"""
+	clarifai_api = ClarifaiApi(app_id='tbndTvx-Mv_OGD4CKeOhPap1gfAFSSWDUzPT2X6x', app_secret='JQNnSLBftBwJLMkNtIsdhUdU7OQ0a5HZDKLdPTtR') # assumes environment variables are set.
+	result = clarifai_api.tag_images(newfood)
+	return result
+
+# def foodItemNum():
+# 	"""find empty item bin number"""
+# 	row=0
+# 	for rows in foodInTheFridge:
+# 		column=1
+# 		for items in rows:
+# 			if items == 0;
+# 				return [column,row]
+# 			column++
+# 		row++
+
+
+# def addFood():
+# 	"""add the food tags and number to the fridge dictionary"""
+# 	foodNum = foodItemNum()
+# 	foodTags = defineFood()
+# 	foodInTheFridge[foodNum] = foodTags #adds the list of food to the array of items in fridge at the foodNum index
+
+def returnFridgeFoodList():
+	"""return list of food available in fridge"""
+
+#def getFood():
+#	""""search for the food in the fridge and return bin number"""
 
 class myFieldStorage(cgi.FieldStorage):
     """Our version uses a named temporary file instead of the default
@@ -86,7 +132,13 @@ class fileUpload:
         # explicitly deletes the original filename
         theFile = formFields['theFile']
         os.link(theFile.file.name, '/Users/maslo/Desktop/SubFridge/tmp/'+theFile.filename)
+        thisFood = open(theFile.filename)
+        im = Image.open(thisFood)
+        im.rotate(45).show()
 
+        print(defineFood(thisFood))
+
+        takePicture()
         return "ok, got it filename='%s'" % theFile.filename
 
 
